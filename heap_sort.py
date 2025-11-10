dfcb7a82762aa6ad45e9c69a1c66a44e78446b44
Marcella Lima <marcellalima990@gmail.com>
2025-11-10 14:05:51 -0300

    heap_sort.py

---
+++ heap_sort.py
@@ -0,0 +1,31 @@
+numeros = [18, 7, 12, 5, 3]
+
+def heapify(lista, n, i):
+    maior = i
+    esquerda = 2 * i + 1
+    direita = 2 * i + 2
+ 
+    if esquerda < n and lista[esquerda] > lista[maior]:
+        maior = esquerda
+    
+    if direita < n and lista[direita] > lista[maior]:
+        maior = direita
+
+    if maior != i:
+        lista[i], lista[maior] = lista[maior], lista[i]
+        heapify(lista, n, maior)
+
+def heap_sort(lista):
+    n = len(lista)
+  
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(lista, n, i)
+
+    for i in range(n - 1, 0, -1):
+        lista[i], lista[0] = lista[0], lista[i] 
+        heapify(lista, i, 0)
+
+    return lista
+
+print("Antes:", numeros)
+print("Depois:", heap_sort(numeros))
